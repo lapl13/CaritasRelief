5710ddc70a11b45146b91c59e47b57b6113c6d57
lapl13 <72638326+lapl13@users.noreply.github.com>
2023-10-20 13:11:37 -0600

    mr1

--- ProyectoCaritas/ProyectoCaritas/LoginWindow.swift
+++ ProyectoCaritas/ProyectoCaritas/LoginWindow.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct LoginWindow: View {
+<<<<<<< Updated upstream
+=======
+    
+    let ColorP = Color(red: 0/255, green: 59/255, blue: 92/255)
+    let ColorS = Color(red: 0/255, green: 156/255, blue: 166/255)
+    @State private var IdUsername: String = ""
+    @State private var IdPassword: String = ""
+    
+    // Offsets for animation
+    @State private var caritas40Offset: CGFloat = UIScreen.main.bounds.width
+    @State private var usernameOffset: CGFloat = -UIScreen.main.bounds.width
+    @State private var passwordOffset: CGFloat = UIScreen.main.bounds.width
+    @State private var buttonOffset: CGFloat = UIScreen.main.bounds.height
+    
+>>>>>>> Stashed changes
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
