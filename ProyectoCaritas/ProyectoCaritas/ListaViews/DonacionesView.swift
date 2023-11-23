10b53bc3f5715c63fff99e9ba98f7dc3d46de083
oskgamart <69476194+oskgamart@users.noreply.github.com>
2023-11-23 13:53:14 -0600

    Refreshable list donaciones

--- ProyectoCaritas/ProyectoCaritas/ListaViews/DonacionesView.swift
+++ ProyectoCaritas/ProyectoCaritas/ListaViews/DonacionesView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 import Foundation
 
-// The notification we'll send when a shake gesture happens.
+
 extension UIDevice {
     static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
 }
 
-//  Override the default behavior of shake gestures to send our notification instead.
+
 extension UIWindow {
      open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
@@ -22,7 +22,7 @@ extension UIWindow {
      }
 }
 
-// A view modifier that detects shaking and calls a function of our choosing.
+
 struct DeviceShakeViewModifier: ViewModifier {
     let action: () -> Void
 
@@ -35,7 +35,7 @@ struct DeviceShakeViewModifier: ViewModifier {
     }
 }
 
-// A View extension to make the modifier easier to use.
+
 extension View {
     func onShake(perform action: @escaping () -> Void) -> some View {
         self.modifier(DeviceShakeViewModifier(action: action))
@@ -98,6 +98,9 @@ struct DonacionesView: View {
                     recibos = getRecibos(token: token, recolector: recolector).recibosActivos
                 }
                 .onShake {
+                    recibos = getRecibos(token: token, recolector: recolector).recibosActivos
+                }
+                .refreshable {
                     recibos = getRecibos(token: token, recolector: recolector).recibosActivos
                 }
                 .listStyle(.plain)
